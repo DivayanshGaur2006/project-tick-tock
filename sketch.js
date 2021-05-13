c3c91b383896d1b522181e5a2da6a06eb2087014
DivayanshGaur2006 <76834576+DivayanshGaur2006@users.noreply.github.com>
2021-05-13 13:36:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,75 @@
+    
+  var hr,mn,sc;
+  var hrAngle,mnAngle,scAngle;  
+    
+  function setup() {
+  createCanvas(400,400);
+
+  // to use angles in degrees
+  angleMode(DEGREES);
+}
+
+  function draw() {
+  background(255,255,255);
+  // translation and rotation
+  translate(200,200);
+  rotate (-90);
+
+  hr = hour();
+  mn = minute();
+  sc = second();
+  // to create maping
+
+  scAngle = map(sc,0,60,0,360);
+  mnAngle = map(mn,0,60,0,360);
+  hrAngle = map(hr%12,0,12,0,360);
+
+  //drawing seconds hand
+   push();
+   rotate(scAngle);
+   //rotate the hand based on angle calculated
+  stroke(255,0,0); 
+  strokeWeight(7);
+  line(0,0,100,0);
+  pop();
+
+    //drawing minute hand
+    push()
+    rotate(mnAngle);
+     // rotate the hand based on angle calculated
+     stroke(0,255,0);
+     strokeWeight(7);
+     line(0,0,75,0);
+     pop()
+
+     //drawing hour hand
+     push()
+     rotate(hrAngle);
+     // rotate the hand based on angle calculated
+     stroke(0,0,255);
+     strokeWeight(7);
+     line(0,0,50,0);
+     pop()
+
+     //drawing the arcs
+
+    strokeWeight(10);
+    noFill();
+
+    //Seconds
+
+    stroke(255,0,0);
+    arc(0,0,300,300,0,scAngle);
+
+    //Minutes
+
+    stroke(0,255,0);
+    arc(0,0,280,280,0,mnAngle);
+
+    //Hour
+
+    stroke(0,0,255);
+    arc(0,0,260,260,0,hrAngle);
+}
+
+  drawSprites();
